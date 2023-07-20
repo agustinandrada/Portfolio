8a242b081665a91d7c8b208eec24dfb76efee278
Agustin <agustinandrada1@gmail.com>
2023-07-20 20:35:01 -0300

    asd

--- src/components/Services.js
+++ src/components/Services.js
@@ -14,13 +14,13 @@ const services = [
   {
     name: "SportWear Ecommerce",
     description:
-      "Final project of the bootcamp, where in a group of 8 people and using agile technologies, we developed an e-commerce, which was a platform for the sale of sports supplies, I participated as a back-end developer, and combined filters were implemented in the application, platform payment, database, cloudinary, local storage, logging system, third-party and local authentication, deployed.",
+      "Platform for selling sports supplies with features like combined filters, platform payment, database integration, Cloudinary for media management, local storage, logging system, and both third-party and local authentication.",
     link: "https://sportwear.vercel.app",
   },
   {
     name: "Dallas Web",
     description:
-      "FA custom-made website for a rustic/neon-themed bar/pub, featuring functionalities such as food ordering and reservations",
+      "A custom-made website for a rustic/neon-themed bar/pub, featuring functionalities such as food ordering and reservations",
     link: "#",
   },
   {
@@ -39,48 +39,56 @@ const services = [
 
 const Services = () => {
   return (
-    <section id="service" className="section">
-      <div className="container mx-auto">
-        <div className="flex flex-col lg:flex-row">
-          {/* text */}
-          <div
-            className="flex-1 lg:bg-services lg:bg-bottom bg-no-repeat mix-blend-lighten
-          mb-12 lg:mb-0"
-          >
-            <h2 className="h2 text-accent mb-6">What I Do.</h2>
-            <h3 className="h3 max-w-[450px] mb-16 ">
-              I'm a passionate Freelance Full Stack Developer{" "}
-            </h3>
+    <section id="service" className="section mt-20">
+      <div className="container mx-auto mt-20">
+        {/* services */}
+        <div className="flex-1">
+          <div className="flex flex-col lg:flex-row">
+            {/* text */}
+            <motion.div
+              variants={fadeIn("right", 0.3)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.3 }}
+              className="flex-1 lg:mb-0 mt-5 py-11"
+            >
+              <h2 className="h2 text-accent ">What I Do.</h2>
+              <h3 className="h3 max-w-[455px]">
+                I'm a passionate Freelance Full Stack Developer{" "}
+              </h3>
+            </motion.div>
           </div>
-        </div>
-        <div>
-          {/* services */}
-          <div className="flex-1">
-            <div>
-              {services.map((service, index) => {
-                const { name, description, link } = service;
-                return (
-                  <div
-                    className="border-b border-white/20 h-[146px] mb-[38px] flex"
-                    key={index}
-                  >
-                    <div className="max-w-[476px]">
-                      <h4 className="text-[20px] tracking-wider font-primary font-semibold mb-6">
-                        {name}
-                      </h4>
-                      <p className="font-secondary leading-tight">
-                        {description}
-                      </p>
-                    </div>
-                    <div>
-                      <a href={link}>
-                        <BsArrowUpRight />
-                      </a>
-                    </div>
+          <div>
+            {services.map((service, index) => {
+              const { name, description, link } = service;
+              return (
+                <motion.div
+                  variants={fadeIn("right", 1)}
+                  initial="hidden"
+                  whileInView={"show"}
+                  viewport={{ once: false, amount: 0.7 }}
+                  className="border-b border-white/20 h-[146px] mb-[10px] flex"
+                  key={index}
+                >
+                  <div className="max-w-[476px] mb-3">
+                    <h4 className="text-[20px] font-primary font-semibold mb-6">
+                      {name}
+                    </h4>
+                    <p className="font-secondary leading-tight mb-3 pb-6">
+                      {description}
+                    </p>
                   </div>
-                );
-              })}
-            </div>
+                  <div className="flex flex-col flex-1 items-end">
+                    <a
+                      href={link}
+                      className="btn w-9 h-9 mb-[42px] flex justify-center items-center"
+                    >
+                      <BsArrowUpRight />
+                    </a>
+                  </div>
+                </motion.div>
+              );
+            })}
           </div>
         </div>
       </div>
